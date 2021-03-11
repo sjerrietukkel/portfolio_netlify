93b4850fa8132567f62e6d0a14a1023fd6200e5b
Daniel van der Schuur <danielvdschuur@gmail.com>
2021-03-11 16:33:39 +0100

    header fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Link } from "react-router-dom"
 import { Nav, NavLink } from "./styled/Header"
+import logo from '../logo.png'; // Tell webpack this JS file uses this image
 
 class Header extends React.Component {
     render () {
         return(
-            <Nav className="navbar navbar-default">
-                <div className="container-fluid d-flex justify-content-end">
-                    <Link className="links" to="/"><NavLink>Home</NavLink></Link>
-                    <Link className="links" to="/about"><NavLink>About</NavLink></Link>
+            <Nav className="navbar navbar-default sticky-top">
+                <div className="container-fluid d-flex justify-content-between nav-max">
+                    <div>
+                        <Link className="links" to="/"><img src={logo} alt="Logo" className="logo" /></Link>
+                    </div>
+                    <div className="d-flex">
+                        <Link className="links" to="/about"><NavLink>About</NavLink></Link>
+                        <Link className="links" to="/"><NavLink>Cases</NavLink></Link>
+                        <Link className="links" to="/"><NavLink>Contact</NavLink></Link>
+                    </div>
                 </div> 
             </Nav>
         );
