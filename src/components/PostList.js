b2dd3c48bb3b585ff941a925bcdbcbced0e121b2
Daniel van der Schuur <danielvdschuur@gmail.com>
2021-03-23 18:55:44 +0100

    fix?

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,13 @@
 import React from "react"
-import Markdown from "react-markdown"
+// import Markdown from "react-markdown"
 import { Link }  from "react-router-dom"
 import postlist from "../posts.json"
 import { Postcard, PostDescription } from "./styled/Postcard"
 
 const PostList = () => {
-    const excerptList = postlist.map(post => {
-        return post.content.split(" ").slice(0, 20).join(" ") + "..."
-    })
+    // const excerptList = postlist.map(post => {
+    //     return post.content.split(" ").slice(0, 20).join(" ") + "..."
+    // })
     return (
         <div className="postlist d-flex justify-content-center flex-wrap">
             {postlist.length && 
@@ -18,7 +18,7 @@ const PostList = () => {
                                 {post.thumbnail && <img className="thumbnail" src={post.thumbnail} alt=""/> }
                             </div>
                             {/* <small>Published on {post.date} by {post.author}</small> */}
-                            <Markdown source={excerptList[i]} escapeHtml={false} />
+                            {/* <Markdown source={excerptList[i]} escapeHtml={false} /> */}
                             <div className="d-flex flex-column">
                                 <h2 className="post-title">{post.title}</h2>
                                 <div>
