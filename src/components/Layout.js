2d9a504363bf9583c70c114c881daf132d0084b4
Daniel van der Schuur <danielvdschuur@gmail.com>
2021-03-23 18:43:29 +0100

    major styling fixes, added about info

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,15 @@
 import React from "react"
 import Header from "./Header"
+import Footer from "./Footer"
 
 const Layout = ({children}) => {
     return (
-        <div className="layout">
+        <div className="layout d-flex flex-column ">
             <Header />
             <div className="layout-body">
                 <div>{children}</div>
             </div>
+            <Footer />
         </div>
     )
 }
