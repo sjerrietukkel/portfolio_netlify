9d99df5acadcee4985e25c70582f00f79ea84bbd
Daniel van der Schuur <danielvdschuur@gmail.com>
2021-03-29 16:04:17 +0200

    styling, new home content

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,7 +8,7 @@ function Home() {
         <main className="col-md-8 col-md-offset-3 mx-lg-auto mx-sm-2 w-lg-75 w-sm-80">
             <div className="about">
               <h3>Hello there 👋,</h3>
-              <p>Ik ben Daniel van der Schuur, een 23 jarige UX designer. Gedurende mijn study Communicatie & Multimedia Design heb ik me gespecialiseerd in oplossingen ontwerpen voor grote, op date gerichtte probleemstukken.</p>
+              <p>Ik ben Daniel van der Schuur, een onlangs afgestudeerde UX designer en opzoek naar werk!</p>
               <div className="social-row">
                 <div className="socials linkedin"><a href="https://www.linkedin.com/in/dani%C3%ABl-van-der-schuur-8b389510a/ " target="_blank" rel="noreferrer">LinkedIn</a></div>   
                 <div className="socials github"><a href="https://www.github.com/sjerrietukkel" target="_blank" rel="noreferrer">Github</a></div> 
